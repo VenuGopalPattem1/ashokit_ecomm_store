6cf88bbe846c4439616c4ed581e269b8a0f53d3b
Venu Gopal <venuv8605@gmail.com>
2026-02-18 06:32:56 +0530

    Update BACKEND_API_URL in constants

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,3 @@
 export const Constants = {
-    BACKEND_API_URL: 'http://a704cc94d0d7649da8a5484c373c0106-283151249.ap-south-1.elb.amazonaws.com',
+    BACKEND_API_URL: 'http://aaddc5456a534479eb277dd1c0843305-2027498708.ap-south-1.elb.amazonaws.com',
 };
